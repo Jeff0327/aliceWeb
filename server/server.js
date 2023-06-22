0f815b56d0947caab36994f009d65ce5e8a6ad6c
김지섭 <cocacola158500@gmail.com>
2023-06-22 20:35:42 +0900

    feat: api/users/signin

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const mongoose = require("mongoose");
 const data = require("./data.js");
 const seedRouter = require("./routes/seedRoutes.js");
 const productRouter = require("./routes/productRoutes.js");
+const userRouter = require("./routes/userRoutes.js");
 const port = process.env.PORT || 5001;
-const app = express();
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -14,10 +15,17 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
