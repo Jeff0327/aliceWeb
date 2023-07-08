40c61bfebcc0a1bac1d0af11501c512554421263
김지섭 <cocacola158500@gmail.com>
2023-07-09 01:48:16 +0900

    delete add123

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -3,7 +3,6 @@ import Spinner from "react-bootstrap/Spinner";
 export default function LoadingBox() {
   return (
     <Spinner animation="border" role="status">
-      <h1>123</h1>
       <span className="visually-hidden">Loading...</span>
     </Spinner>
   );
