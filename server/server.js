cb8bf3f7d412f8babc5c31101f395679ca6e2399
김지섭 <cocacola158500@gmail.com>
2023-07-09 03:00:34 +0900

    1

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const seedRouter = require("./routes/seedRoutes.js");
@@ -28,6 +29,11 @@ app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
