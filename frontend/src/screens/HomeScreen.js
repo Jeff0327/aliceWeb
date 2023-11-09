a759e26b653fb840c2a0adcf2504dedefbb22d83
김지섭 <cocacola158500@gmail.com>
2023-11-09 17:51:00 +0900

    homeScreen headers

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -33,7 +33,14 @@ export default function HomeScreen() {
     const fetchData = async () => {
       dispatch({ type: "FETCH_REQUEST" });
       try {
-        const result = await axios.get("/api/products");
+        const result = await axios.get("/api/products", {
+          headers: {
+            "Access-Control-Allow-Origin": [
+              "http://localhost:3000",
+              "https://rosemarry.kr",
+            ],
+          },
+        });
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
         dispatch({ type: "FETCH_FAIL", payload: err.message });
