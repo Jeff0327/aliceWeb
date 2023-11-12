9ca70f7d52a71a05830b61387f3b65b9a7f70b33
김지섭 <cocacola158500@gmail.com>
2023-11-12 11:35:59 +0900

    Access fix

--- server/server.js
+++ server/server.js
@@ -28,12 +28,14 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*",
-    "Cross-Origin-Opener-Policy": "same-origin",
-    "Cross-Origin-Embedder-Policy": "require-corp",
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Cross-Origin-Opener-Policy", [
+    "same-origin",
+    "same-origin-allow-popups",
+  ]);
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+
   next();
 });
 // app.use((req, res, next) => {
