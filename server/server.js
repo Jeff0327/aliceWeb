41e9aa3481d0a8f968e5d93ff1d4f2ce2f9aca15
김지섭 <cocacola158500@gmail.com>
2023-11-12 17:06:34 +0900

    coep del

--- server/server.js
+++ server/server.js
@@ -31,7 +31,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
 
   next();
 });
