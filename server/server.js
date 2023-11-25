14df2047ef50ff73c393935ef67025d38f09d601
김지섭 <cocacola158500@gmail.com>
2023-11-25 18:21:32 +0900

    del app options

--- server/server.js
+++ server/server.js
@@ -31,14 +31,14 @@ app.use(
     credentials: true,
   })
 );
-app.options("*", cors());
+
 app.use((req, res, next) => {
   console.log("Received request:", req.method, req.url);
   res.header("Access-Control-Allow-Origin", "*"); // or specific origin
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("same-origin, same-origin-allow-popups");
+
   next();
 });
 app.use(express.json());
