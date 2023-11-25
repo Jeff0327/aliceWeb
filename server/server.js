0fc5ca2aa85cb8a3ffaf04c0198ce6cad25ecb96
김지섭 <cocacola158500@gmail.com>
2023-11-25 18:31:15 +0900

    cors origin setup

--- server/server.js
+++ server/server.js
@@ -20,27 +20,18 @@ mongoose
   });
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000/",
-      "http://localhost:3000",
-      "https://rosemarry.kr",
-    ],
-    methods: "*",
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: [
+    "http://localhost:3000/",
+    "http://localhost:3000",
+    "https://rosemarry.kr",
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  console.log("Received request:", req.method, req.url);
-  res.header("Access-Control-Allow-Origin", "*"); // or specific origin
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
-
-  next();
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
