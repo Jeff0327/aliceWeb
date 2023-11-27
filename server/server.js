10095f97479fa6be1004d38ed346110299563bd4
김지섭 <cocacola158500@gmail.com>
2023-11-27 13:27:57 +0900

    cors array

--- server/server.js
+++ server/server.js
@@ -21,29 +21,13 @@ mongoose
 const app = express();
 
 const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "https://rosemarry.kr",
-    "http://localhost:3000/",
-  ],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  origin: [["http://localhost:3000", "https://rosemarry.kr"]],
+  methods: "*",
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "http://localhost:3000",
-    "https://rosemarry.kr",
-    "http://localhost:3000/"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
