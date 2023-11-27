bb79dcc10be2acd1f1609d1c1aaef2b12b73bfef
김지섭 <cocacola158500@gmail.com>
2023-11-27 20:08:26 +0900

    cors array

--- server/server.js
+++ server/server.js
@@ -28,11 +28,10 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
+  res.header("Access-Control-Allow-Origin", [
     "http://localhost:3000",
-    "https://rosemarry.kr"
-  );
+    "https://rosemarry.kr",
+  ]);
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", true);
