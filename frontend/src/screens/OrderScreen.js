3b99482f425da1990e049f7148c6b1b143343a05
김지섭 <cocacola158500@gmail.com>
2023-11-28 22:58:57 +0900

    await bootpayresult

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -114,6 +114,62 @@ export default function OrderScreen() {
     toast.error(getError(err));
   }
 
+  const bootpayResult = async () => {
+    try {
+      const response = await Bootpay.requestPayment({
+        //- 요청 데이터
+      });
+      switch (response.event) {
+        case "issued":
+          // 가상계좌 입금 완료 처리
+          break;
+        case "done":
+          console.log(response);
+          // 결제 완료 처리
+          break;
+        case "confirm": //payload.extra.separately_confirmed = true; 일 경우 승인 전 해당 이벤트가 호출됨
+          console.log(response.receipt_id);
+          /**
+           * 1. 클라이언트 승인을 하고자 할때
+           * // validationQuantityFromServer(); //예시) 재고확인과 같은 내부 로직을 처리하기 한다.
+           */
+
+          const confirmedData = await Bootpay.confirm(); //결제를 승인한다
+          if (confirmedData.event === "done") {
+            //결제 성공
+          }
+
+          /**
+           * 2. 서버 승인을 하고자 할때
+           * // requestServerConfirm(); //예시) 서버 승인을 할 수 있도록  API를 호출한다. 서버에서는 재고확인과 로직 검증 후 서버승인을 요청한다.
+           * Bootpay.destroy(); //결제창을 닫는다.
+           */
+          break;
+        default:
+          // Handle other cases if needed
+          break;
+      }
+    } catch (e) {
+      // // 결제 진행중 오류 발생
+      // e.error_code - 부트페이 오류 코드
+      // e.pg_error_code - PG 오류 코드
+      // e.message - 오류 내용
+      console.log(e.message);
+      switch (e.event) {
+        case "cancel":
+          // 사용자가 결제창을 닫을때 호출
+          console.log(e.message);
+          break;
+        case "error":
+          // 결제 승인 중 오류 발생시 호출
+          console.log(e.error_code);
+          break;
+        default:
+          // Handle other cases if needed
+          break;
+      }
+    }
+  };
   const bootpayhandler = async () => {
     try {
       await Bootpay.requestPayment({
@@ -135,6 +191,7 @@ export default function OrderScreen() {
           escrow: false,
         },
       });
+      await bootpayResult();
     } catch (err) {
       console.log(err);
     }
@@ -335,7 +392,8 @@ export default function OrderScreen() {
                             <Col>
                               <Button
                                 type="button"
-                                value="success"
+                                variant="success"
+                                className="btn btn-success btn-width"
                                 onClick={bootpayhandler}
                               >
                                 결제하기
