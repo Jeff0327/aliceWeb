bfecd24c7d91670d18a7af685f8c9560ad672142
김지섭 <cocacola158500@gmail.com>
2023-11-30 20:16:01 +0900

    object add apiKey

--- frontend/src/components/KakaoLoginButton.js
+++ frontend/src/components/KakaoLoginButton.js
@@ -3,7 +3,7 @@ import KakaoLogin from "react-kakao-login";
 const KakaoLoginButton = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      window.Kakao.init(`${process.env.KAKAO_JAVASCRIPT_KEY}`);
+      window.Kakao.init({ apiKey: `${process.env.KAKAO_JAVASCRIPT_KEY}` });
     }
   }, []);
   const kakaoOnSuccess = () => {
