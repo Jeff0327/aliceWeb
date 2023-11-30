cffbcadd672cef36836bfc8c9f8cd7d47025a5fd
김지섭 <cocacola158500@gmail.com>
2023-11-30 20:05:39 +0900

    kakao login

--- frontend/src/components/KakaoLoginButton.js
+++ frontend/src/components/KakaoLoginButton.js
@@ -1,24 +1,33 @@
-import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
 import KakaoLogin from "react-kakao-login";
 const KakaoLoginButton = () => {
-  const kakaoOnSuccess = async (e) => {
-    const { data } = await axios.post("/api/kakaosignin", {
-      headers: { Authorization: `Bearer ${process.env.KAKAO_JAVASCRIPT_KEY}` },
-    });
-    const idToken = e.response.access_token;
-    console.log(data);
-    console.log(idToken);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.Kakao.init(process.env.KAKAO_JAVASCRIPT_KEY);
+    }
+  }, []);
+  const kakaoOnSuccess = () => {
+    console.log("로그인 성공");
   };
   const kakaoOnFailure = (error) => {
-    console.log(error);
+    console.log("로그인 실패:", error);
   };
   return (
     <>
       <KakaoLogin
-        token={`F7B06E3AC96D23F77CFB`}
+        token={process.env.KAKAO_JAVASCRIPT_KEY}
         onSuccess={kakaoOnSuccess}
         onFail={kakaoOnFailure}
+        render={({ onClick }) => (
+          <div
+            onClick={(e) => {
+              e.preventDefault();
+              onClick();
+            }}
+          >
+            카카오로 로그인하기
+          </div>
+        )}
       />
     </>
   );
