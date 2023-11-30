421150f4e90b9725fd174d5388e9ab133960ed29
김지섭 <cocacola158500@gmail.com>
2023-11-30 20:30:24 +0900

    change javascript key

--- frontend/src/components/KakaoLoginButton.js
+++ frontend/src/components/KakaoLoginButton.js
@@ -3,7 +3,7 @@ import KakaoLogin from "react-kakao-login";
 const KakaoLoginButton = () => {
   useEffect(() => {
     if (typeof window !== "undefined" && window.kakao) {
-      window.Kakao.init({ apiKey: `${process.env.KAKAO_RESTAPI_KEY}` });
+      window.Kakao.init({ apiKey: `${process.env.KAKAO_JAVASCRIPT_KEY}` });
     }
   }, []);
   const kakaoOnSuccess = () => {
@@ -14,7 +14,7 @@ const KakaoLoginButton = () => {
   };
   return (
     <KakaoLogin
-      token={`${process.env.KAKAO_RESTAPI_KEY}`}
+      token={`${process.env.KAKAO_JAVASCRIPT_KEY}`}
       onSuccess={kakaoOnSuccess}
       onFail={kakaoOnFailure}
       render={({ onClick }) => (
