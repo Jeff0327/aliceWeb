237d1db37b4fcc42754d93e998c119cb8d3b465a
김지섭 <cocacola158500@gmail.com>
2023-12-08 07:21:27 +0900

    coop unsafe-none

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "*");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  res.header("Cross-Origin-Opener-Policy", "unsafe-none");
   next();
 });
 app.use(express.json());
