59ecbe21be69e38808fd04d70e89a0a40d8683e3
김지섭 <cocacola158500@gmail.com>
2023-12-08 07:08:22 +0900

    same-origin

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "*");
-  res.header("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
 app.use(express.json());
