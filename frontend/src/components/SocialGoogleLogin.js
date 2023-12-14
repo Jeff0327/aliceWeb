6fd58eb2bba15b7bb241343b97d3a19855f653e2
김지섭 <cocacola158500@gmail.com>
2023-12-14 23:20:11 +0900

    google auth

--- frontend/src/components/SocialGoogleLogin.js
+++ frontend/src/components/SocialGoogleLogin.js
@@ -1,16 +1,36 @@
 import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
+
 const SocialGoogleLogin = () => {
   const googleSocialLogin = useGoogleLogin({
     onSuccess: async (res) => {
-      console.log("codeResponse:", res.code);
+      console.log("Authorization Code:", res.code);
 
-      const accessToken = res.code;
-      await getUserInfo(accessToken);
+      try {
+        const response = await axios.post(
+          "https://www.googleapis.com/oauth2/v4/token",
+          {
+            code: res.code,
+            client_id:
+              "258796595331-i3a9759p2fjajsg80gr3fsuavbdko1ld.apps.googleusercontent.com",
+            client_secret: "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",
+            redirect_uri: "http://localhost:3000/googleLoginBtn",
+            grant_type: "authorization_code",
+          }
+        );
+
+        const accessToken = response.data.access_token;
+        console.log("Access Token:", accessToken);
+
+        // Now you can use the access token to fetch user information
+        await getUserInfo(accessToken);
+      } catch (error) {
+        console.error("Error exchanging code for access token:", error);
+      }
     },
-
     flow: "auth-code",
   });
+
   const getUserInfo = async (accessToken) => {
     try {
       const response = await axios.get(
@@ -18,15 +38,13 @@ const SocialGoogleLogin = () => {
       );
       console.log(response);
 
-      // Access user information in the response.data object
       const userInfo = response.data;
       console.log("User Info:", userInfo);
-
-      // Now you can use the user information as needed
     } catch (error) {
-      console.error("Error fetching user information:", error);
+      console.error("Error fetching user information:", error.response.data);
     }
   };
+
   return (
     <div className="social-login-container" onClick={() => googleSocialLogin()}>
       <div>
