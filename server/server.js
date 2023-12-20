45e8f5f5be40afacf05ba31aac72c2a13b935361
김지섭 <cocacola158500@gmail.com>
2023-12-21 01:28:32 +0900

    server

--- server/server.js
+++ server/server.js
@@ -39,8 +39,7 @@ app.use((req, res, next) => {
     "same-origin",
     "same-origin-allow-popups",
   ]);
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+
   next();
 });
 app.use(express.json());
