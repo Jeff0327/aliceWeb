a5ea46095ede1eae53034c0a2dbc3c5053f4c171
김지섭 <cocacola158500@gmail.com>
2023-12-22 15:44:23 +0900

    fix header

--- server/server.js
+++ server/server.js
@@ -38,13 +38,17 @@ app.use((req, res, next) => {
   }
 
   const allowedHeaders = [
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization",
   ];
-  const header = req.headers.header;
-  if (allowedHeaders.includes(header)) {
-    res.setHeader("Access-Control-Allow-Headers", header);
-  }
+  allowedHeaders.forEach((header) => {
+    if (req.headers[header]) {
+      res.setHeader("Access-Control-Allow-Headers", header);
+    }
+  });
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Credentials", true);
 
