0b9fdb1da24df1875d5518c135fa30bc85cec9af
김지섭 <cocacola158500@gmail.com>
2023-12-23 19:26:27 +0900

    header

--- server/server.js
+++ server/server.js
@@ -50,13 +50,7 @@ app.use((req, res, next) => {
   //   }
   // });
   res.header(
-    "Access-Control-Allow-Headers",
-    "content-type",
-    "Origin",
-    "X-Api-Key",
-    "X-Requested-With",
-    "Accept",
-    "Authorization"
+    "Access-Control-Allow-Headers,content-type,Origin,X-Api-Key,X-Requested-With,Accept,Authorization"
   );
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Credentials", true);
