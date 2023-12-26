72f52a9adcce01dc800514641d0d214f3b311e11
김지섭 <cocacola158500@gmail.com>
2023-12-26 19:35:51 +0900

    header

--- server/server.js
+++ server/server.js
@@ -37,7 +37,10 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin"
+  );
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Credentials", true);
 
