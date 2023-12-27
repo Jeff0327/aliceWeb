e7983f3064ec55486c82ccf5e083c1c956733e93
김지섭 <cocacola158500@gmail.com>
2023-12-28 05:12:06 +0900

    결제하기 ui

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -378,21 +378,17 @@ export default function OrderScreen() {
                             onError={onError}
                           ></PayPalButtons>
                         ) : (
-                          <Row className="w-100">
-                            <Col>
-                              <div className="d-grid">
-                                <Button
-                                  type="button"
-                                  variant="success"
-                                  className="btn btn-success btn-width"
-                                  onClick={bootpayhandler}
-                                  style={{ width: 100 }}
-                                >
-                                  결제하기
-                                </Button>
-                              </div>
-                            </Col>
-                          </Row>
+                          <div className="d-grid">
+                            <Button
+                              type="button"
+                              variant="success"
+                              className="btn btn-success btn-width"
+                              onClick={bootpayhandler}
+                              style={{ width: 100 }}
+                            >
+                              결제하기
+                            </Button>
+                          </div>
                         )}
                       </div>
                     )}
