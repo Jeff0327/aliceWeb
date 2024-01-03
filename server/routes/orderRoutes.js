1a77837b5ce4db45404c242d969761f160d8c21b
김지섭 <cocacola158500@gmail.com>
2024-01-04 03:52:15 +0900

    kakaoUser

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -61,7 +61,7 @@ orderRouter.post(
         itemsPrice: req.body.itemsPrice,
         shippingPrice: req.body.shippingPrice,
         totalPrice: req.body.totalPrice,
-        kakaoUser: req.kakaoUser,
+        kakaoUser: { _id: kakaoUser._id, name: kakaoUser.email },
       });
       const order = await newOrder.save();
 
